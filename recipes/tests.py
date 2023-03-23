422ec7a8df1bc0ed9118391e3575e40d5d2063b7
Jhones <jhonesaly@gmail.com>
2023-03-23 08:51:54 -0300

    test: add test for view of category

--- recipes/tests.py
+++ recipes/tests.py
@@ -23,3 +23,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
