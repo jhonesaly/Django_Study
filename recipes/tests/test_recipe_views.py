b96d9974956f8bf8d5c168b56756e64c13ec30b5
Jhones <jhonesaly@gmail.com>
2023-03-23 09:44:04 -0300

    test: add test for response 404 for no recipe

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -36,3 +36,9 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
+        self.assertEqual(response.status_code, 404)
