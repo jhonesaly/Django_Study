cbe976634a252da9ee26e0d06ecb8986a86fe5b6
Jhones <jhonesaly@gmail.com>
2023-03-23 08:08:59 -0300

    refactor: change name of var to generic

--- recipes/tests.py
+++ recipes/tests.py
@@ -5,5 +5,5 @@ from django.urls import reverse
 class RecipeURLsTest(TestCase):
 
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
