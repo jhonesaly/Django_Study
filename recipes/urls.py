d51d0a1596a026c6d03f91eba82199b327cba3d4
Jhones <jhonesaly@gmail.com>
2023-03-23 15:34:56 -0300

    feat: add search url

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/search/', lambda request: ..., name="search"),
 ]
