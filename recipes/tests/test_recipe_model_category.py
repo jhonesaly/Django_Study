7b1d2241b222340d41f89f917d4b01a963db2d85
Jhones <jhonesaly@gmail.com>
2023-03-23 14:34:25 -0300

    test: file for model category

---
+++ recipes/tests/test_recipe_model_category.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeCategoryModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.category = self.make_category(
+            name='Category Testing'
+        )
+        return super().setUp()
