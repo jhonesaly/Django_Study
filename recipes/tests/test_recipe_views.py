24fc30d8c1c283a5c6531e73c448a62a435c6ac6
Jhones <jhonesaly@gmail.com>
2023-03-23 09:21:31 -0300

    test: add test for correct template of home view

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -20,3 +20,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
