8e17cde65d783a44168c33a11868766efda84041
Jhones <jhonesaly@gmail.com>
2023-03-23 08:13:22 -0300

    test: add test of category url

--- recipes/tests.py
+++ recipes/tests.py
@@ -7,3 +7,7 @@ class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         url = reverse('recipes:home')
         self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
