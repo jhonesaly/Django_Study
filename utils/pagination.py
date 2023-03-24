6a2ac9b704b1fe9c38753ee0bce97bf012fef4e3
Jhones <jhonesaly@gmail.com>
2023-03-24 16:29:40 -0300

    feat: return key:values of function

--- utils/pagination.py
+++ utils/pagination.py
@@ -21,4 +21,14 @@ def make_pagination_range(
         start_range = start_range - abs(total_pages - stop_range)
 
     pagination = page_range[start_range:stop_range]
-    return pagination
+    return {
+        'pagination': pagination,
+        'page_range': page_range,
+        'qty_pages': qty_pages,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start_range,
+        'stop_range': stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range': stop_range < total_pages,
+    }
