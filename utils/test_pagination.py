e1e54ca8de20bd9e4c553f5ae23ebea55a6e1701
Jhones <jhonesaly@gmail.com>
2023-03-24 16:07:48 -0300

    test: pagination is static if current less than 4

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -11,3 +11,12 @@ class PaginationTest(TestCase):
             current_page=1,
         )
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+        # Current page = 1 - Qty Page = 2 - Middle Page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
