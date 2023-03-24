07d3140b06a08ce01159fd18309b33f80872b9f4
Jhones <jhonesaly@gmail.com>
2023-03-24 16:28:28 -0300

    feat: pagination of the end

--- utils/pagination.py
+++ utils/pagination.py
@@ -9,6 +9,7 @@ def make_pagination_range(
     middle_range = math.ceil(qty_pages / 2)
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
+    total_pages = len(page_range)
 
     start_range_offset = abs(start_range) if start_range < 0 else 0
 
@@ -16,4 +17,8 @@ def make_pagination_range(
         start_range = 0
         stop_range += start_range_offset
 
-    return page_range[start_range:stop_range]
+    if stop_range >= total_pages:
+        start_range = start_range - abs(total_pages - stop_range)
+
+    pagination = page_range[start_range:stop_range]
+    return pagination
