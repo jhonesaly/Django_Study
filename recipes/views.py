aa82450956649d150b74c795d355fd541a4804df
Jhones <jhonesaly@gmail.com>
2023-03-24 18:42:13 -0300

    feat: uses environment variables in views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -5,7 +7,7 @@ from utils.pagination import make_pagination
 
 from recipes.models import Recipe
 
-PER_PAGE = 9
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def home(request):
