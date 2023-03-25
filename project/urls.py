a1929564540840ce4ff472b464851a6807dc776d
Jhones <jhonesaly@gmail.com>
2023-03-25 11:22:31 -0300

    feat: add authors app to url patterns of project

--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),  # domínio.com/
-
+    path('authors/', include('authors.urls')),
     # path('urls/', include('recipes.urls')), # domínio.com/urls
 ]
 
