1d2f7c89847b88a2cccbffd92e83cd16f0ed35d4
Jhones <jhonesaly@gmail.com>
2023-03-25 16:31:54 -0300

    test: first name placeholder is correct

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,5 +1,9 @@
+from authors.forms import RegisterForm
 from django.test import TestCase
 
 
 class AuthorRegisterFormUnitTest(TestCase):
-    ...
+    def test_first_name_placeholder_is_correct(self):
+        form = RegisterForm()
+        placeholder = form['first_name'].field.widget.attrs['placeholder']
+        self.assertEqual('Ex.: John', placeholder)
