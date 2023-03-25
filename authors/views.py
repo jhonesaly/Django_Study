a76f747283d9957704d7840b552b40e3be76ac95
Jhones <jhonesaly@gmail.com>
2023-03-25 14:18:47 -0300

    feat: if register ok, clean page

--- authors/views.py
+++ authors/views.py
@@ -1,13 +1,12 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    request.session['number'] = request.session.get('number') or 1
-    request.session['number'] += 1
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -17,7 +16,8 @@ def register_create(request):
     if not request.POST:
         raise Http404()
 
+    POST = request.POST
+    request.session['register_form_data'] = POST
     form = RegisterForm(POST)
-    return render(request, 'authors/pages/register_view.html', {
-        'form': form,
-    })
+
+    return redirect('authors:register')
