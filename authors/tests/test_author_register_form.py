76f64d340894ea52112d676835d4591d799b3610
Jhones <jhonesaly@gmail.com>
2023-03-28 09:32:55 -0300

    fix: update name of test

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -12,7 +12,7 @@ class AuthorRegisterFormUnitTest(TestCase):
         ('password', 'Type your password'),
         ('password2', 'Repeat your password'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder(self, field, placeholder):
         form = RegisterForm()
         current_placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(current_placeholder, placeholder)
