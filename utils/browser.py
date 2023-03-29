5a8a21c44555585f17915e2379d96977bb7460a1
Jhones <jhonesaly@gmail.com>
2023-03-29 06:41:23 -0300

    feat: turn automation into function

--- utils/browser.py
+++ utils/browser.py
@@ -8,10 +8,15 @@ ROOT_PATH = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = 'chromedriver'
 CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
 
-chrome_options = webdriver.ChromeOptions()
-chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
-browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-browser.get('http://www.google.com.br/')
-sleep(5)
-browser.quit()
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+
+if __name__ == '__main__':
+    browser = make_chrome_browser()
+    browser.get('http://www.google.com.br/')
+    sleep(5)
+    browser.quit()
