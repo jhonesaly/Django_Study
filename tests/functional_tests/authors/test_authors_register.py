56ac382452cbf74bfb14a69f869d1d849095e692
Jhones <jhonesaly@gmail.com>
2023-03-29 11:25:18 -0300

    test: functional empty username

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -51,3 +51,12 @@ class AuthorsRegisterTest(AuthorsBaseTest):
             form = self.get_form()
             self.assertIn('Write your last name', form.text)
         self.form_field_test_with_callback(callback)
+
+    def test_empty_username_error_message(self):
+        def callback(form):
+            username_field = self.get_by_placeholder(form, 'Your username')
+            username_field.send_keys(' ')
+            username_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('This field must not be empty', form.text)
+        self.form_field_test_with_callback(callback)
