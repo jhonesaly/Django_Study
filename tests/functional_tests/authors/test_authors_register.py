e76b0403b6f52aff741e0ac09f6ed91cfea46ef0
Jhones <jhonesaly@gmail.com>
2023-03-29 10:56:59 -0300

    test: configure author functional tests

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,7 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from .base import AuthorsBaseTest
 
 
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def test_the_test(self):
-        self.browser.get(self.live_server_url + '/authors/register/')
-        self.sleep()
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
+
+    def fill_form_dummy_data(self, form):
+
+        fields = form.find_elements(By.TAG_NAME, 'input')
+
+        for field in fields:
+            if field.is_displayed():
+                field.send_keys(' ' * 20)
