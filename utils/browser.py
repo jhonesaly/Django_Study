36115db37b0dc00bb2cafd9fadaaac5f4109ee72
Jhones <jhonesaly@gmail.com>
2023-03-29 06:45:20 -0300

    feat: iterate options in function

--- utils/browser.py
+++ utils/browser.py
@@ -11,12 +11,18 @@ CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
 
 def make_chrome_browser(*options):
     chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return browser
 
 
 if __name__ == '__main__':
-    browser = make_chrome_browser()
-    browser.get('http://www.google.com.br/')
+    browser = make_chrome_browser('--headless')
+    browser.get('http://www.udemy.com/')
     sleep(5)
     browser.quit()
