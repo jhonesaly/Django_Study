8f46fb93a486b1eefdd01846b66f709267e4253a
Jhones <jhonesaly@gmail.com>
2023-03-29 11:22:46 -0300

    test: refactor empty first name test

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -35,19 +35,10 @@ class AuthorsRegisterTest(AuthorsBaseTest):
         return form
 
     def test_empty_first_name_error_message(self):
-        self.browser.get(self.live_server_url + '/authors/register/')
-        form = self.get_form()
-
-        self.fill_form_dummy_data(form)
-        form.find_element(By.NAME, 'email').send_keys('dummy@email.com')
-
-        first_name_field = self.get_by_placeholder(form, 'Ex.: John')
-        first_name_field.send_keys(' ')
-        first_name_field.send_keys(Keys.ENTER)
-
-        form = self.browser.find_element(
-            By.XPATH,
-            '/html/body/main/div[2]/form'
-        )
-
-        self.assertIn('Write your first name', form.text)
+        def callback(form):
+            first_name_field = self.get_by_placeholder(form, 'Ex.: John')
+            first_name_field.send_keys(' ')
+            first_name_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Write your first name', form.text)
+        self.form_field_test_with_callback(callback)
