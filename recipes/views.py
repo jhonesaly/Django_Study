c8d0ec1885efd8ddb529ccb42ebecbd182dd80e6
Jhones <jhonesaly@gmail.com>
2023-03-30 18:02:58 -0300

    feat: CBV of search view

--- recipes/views.py
+++ recipes/views.py
@@ -52,6 +52,33 @@ class RecipeListViewCategory(RecipeListViewBase):
         return qs
 
 
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            )
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
+        return ctx
+
+
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
