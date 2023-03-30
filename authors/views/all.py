d0417aa5f5256754179203bf94618e83753a9504
Jhones <jhonesaly@gmail.com>
2023-03-30 17:05:43 -0300

    refactor: remove code moved to CBV

--- authors/views/all.py
+++ authors/views/all.py
@@ -1,5 +1,4 @@
 from authors.forms import LoginForm, RegisterForm
-from authors.forms.recipe_form import AuthorRecipeForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -95,37 +94,6 @@ def dashboard(request):
         'authors/pages/dashboard.html',
         context={
             'recipes': recipes,
-        }
-    )
-
-
-@login_required(login_url='authors:login', redirect_field_name='next')
-def dashboard_recipe_new(request):
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-    )
-
-    if form.is_valid():
-        recipe: Recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-
-        recipe.save()
-
-        messages.success(request, 'Salvo com sucesso!')
-        return redirect(
-            reverse('authors:dashboard_recipe_edit', args=(recipe.id,))
-        )
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form,
-            'form_action': reverse('authors:dashboard_recipe_new')
         }
     )
 
