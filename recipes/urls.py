b65515dacf90ffaa4451619752401d9bc1a41a5e
Jhones <jhonesaly@gmail.com>
2023-03-30 18:03:37 -0300

    feat: url of search CBV

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,10 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name="home"),
-    path('recipes/search/', views.search, name="search"),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(), name="search"
+    ),
     path(
         'recipes/category/<int:category_id>/',
         views.RecipeListViewCategory.as_view(), name="category"
